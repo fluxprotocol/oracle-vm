efe454024c42d8da35b862d97c875053d507beb7
Franklin Waller <fr.l.waller@gmail.com>
2021-11-04 09:19:30 +0100

    chore(example): Remove example in favor of it's own repo

--- examples/simple-url-call/scripts/build.sh
+++
@@ -1,3 +0,0 @@
-cargo build --target wasm32-wasi --release
-wasm-opt -Oz -o ./target/wasm32-wasi/release/basic-fetch.wasm ./target/wasm32-wasi/release/simple-url-call.wasm
-cp ./target/wasm32-wasi/release/basic-fetch.wasm ../../test/wasm
